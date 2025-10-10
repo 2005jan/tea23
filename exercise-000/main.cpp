1d912bd8d6edbd999f091cf43647b40c5d5e89af
Jan Hafen <157302017+2005jan@users.noreply.github.com>
2025-10-10 11:41:29 +0200

    Aufgabe 0

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -1,7 +1,7 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
 
-#include "config.h"
+#include "config.h.in"
 
 int main(int argc, char **argv) {
 
@@ -11,6 +11,11 @@ int main(int argc, char **argv) {
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", tea::PROJECT_NAME);
+    fmt::print("Anzahl Argument (argc): {}\n",argc);
+    for (size_t i = 0; i<argc; i++)
+    {
+        fmt::print("argv[{}]: {} | {}\n",i,argv[i], fmt::ptr(&argv[i]));
+    }
 
     /* INSERT YOUR CODE HERE */
 
