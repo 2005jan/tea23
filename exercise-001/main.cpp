4504b9db8863f97290039dcd4089a7d796d65b67
Jan Hafen <157302017+2005jan@users.noreply.github.com>
2025-10-17 14:08:46 +0200

    Part 2

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -3,6 +3,11 @@
 
 #include "config.h"
 
+int globalVar = 1;
+int static staticVar = 1;
+int const constVar = 1;
+int* heapVar = new int(3);
+
 int main(int argc, char **argv) {
 
     /**
@@ -13,6 +18,20 @@ int main(int argc, char **argv) {
     fmt::print("Hello, {}!\n", tea::PROJECT_NAME);
 
     /* INSERT YOUR CODE HERE */
+    
+    int localVar = 1;
+    int static localstaticVar = 1;
+    int const localconstVar = 1;
+    int* localheapVar = new int(3);
+
+    fmt::print("Wert: {}, Adresse: {}\n", globalVar, fmt::ptr(&globalVar));
+    fmt::print("Wert: {}, Adresse: {}\n", staticVar, fmt::ptr(&staticVar));
+    fmt::print("Wert: {}, Adresse: {}\n", constVar, fmt::ptr(&constVar));
+    //fmt::print("Wert: {}, Adresse: {}\n", &heapVar, fmt::ptr(heapVar));
+    fmt::print("Wert: {}, Adresse: {}\n", localVar, fmt::ptr(&localVar));
+    fmt::print("Wert: {}, Adresse: {}\n", localstaticVar, fmt::ptr(&localstaticVar));
+    fmt::print("Wert: {}, Adresse: {}\n", localconstVar, fmt::ptr(&localconstVar));
+    //fmt::print("Wert: {}, Adresse: {}\n", &localheapVar, fmt::ptr(localheapVar));
 
     return 0; /* exit gracefully*/
 }
