57523cb9b226d907f1edd32e0843ec2c56bfbb73
Jan Hafen <157302017+2005jan@users.noreply.github.com>
2025-11-14 14:10:49 +0100

    feat: add exercise number three

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -1,6 +1,20 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
 
+typedef enum{
+    OBEN = 0,
+    UNTEN = 1,
+    HOCH = 2,
+    RUNTER = 3,
+    FEHLER = 4,
+} States;
+
+typedef enum{
+    TASTE = 0,
+    ES_O = 1,
+    ES_U = 2,
+} Events;
+
 auto main(int argc, char** argv) -> int
 {
     /**
